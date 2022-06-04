4ab65ee4c50e5da112f9e4ea35e0d55898d2e782
Eliana <elianacristaldoo@gmail.com>
2022-06-04 18:03:49 -0300

    Agrega js

---
+++ app.js
@@ -0,0 +1,87 @@
+//calcular edad promedio de personas registrada
+
+// alert("Escriba 5(cinco) edades y se le devolverá el resultado")
+//    let edad1 = Number(prompt("Escriba edad"));
+//    let edad2 = Number(prompt("Escriba edad"));
+//    let edad3 = Number(prompt("Escriba edad"));
+//    let edad4 = Number(prompt("Escriba edad"));
+//    let edad5 = Number(prompt("Escriba edad"));
+
+//  function edadPromedio(edad1, edad2, edad3, edad4, edad5){
+//      let suma = Math.round((edad1 + edad2 + edad3 + edad4 + edad5) / 5);
+//      alert("La edad promedio de los usuarios ingresados es " + suma)
+//  }alert (edadPromedio(edad1, edad2, edad3, edad4, edad5))
+
+ //En el ejercicio de arriba intenté crear una función donde el usuario ingrese la 
+ //cantidad de personas que van a escribir su edad utilizando "for" pero no me salió
+ //dado que sólo dividía la cantidad ingresada al final. Me sale un "undefined" al fi
+
+
+
+//calcular costo total de productos seleccionados por el cliente
+
+//   let producto= parseInt(prompt("escribe 1 para alfajor, 2 para galletita, 3 para banana, 4 fideo"));
+
+//   let precio1= 100;
+//   let precio2= 150;
+//   let precio3= 50;
+//   let precio4= 300;
+//   let esc= " "
+
+//   function lista (producto, precio1, precio2, precio3, precio4, esc){
+//       while(producto !== esc){
+//           if (producto == 1){
+//               document.write(precio1 + "<br>")
+//           }
+//           else if (producto == 2){
+//               document.write(precio2 + "<br>")
+//       }
+//           else if (producto == 3){
+//               document.write(precio3 + "<br>")
+//           }
+//           else if (producto == 4){
+//               document.write(precio4 + "<br>")
+//           }
+//           else{
+//               alert("este producto no pertenece a la lista")
+//           }
+//           producto=prompt("escribe 1 para alfajor, 2 para galletita, 3 para banana, 4 fideo o espacio para salir");
+//       }
+//  }
+//  alert(lista(producto, precio1, precio2, precio3, precio4, esc))
+
+//Misma situación que la función anterior. No logro crear buen una devolución de la suma
+
+
+
+
+//calcular valor final de un prdocto seleccionado en función de impuestos y descuento
+
+// let precio= Number(prompt("Escriba el precio de su producto"));
+// let desc30= 0.30;
+// let desc20= 0.20;
+// let detener= " ";
+
+// function descuento(precio, desc30, desc20, detener){
+//     while (precio != detener){
+//         if(precio>=1000){
+//             let desc1 = (precio * desc30);
+//             document.write ((precio - desc1) + "<br>")
+//         }else if((precio < 1000) && (precio >= 500)){
+//              let desc2 = (precio * desc20);
+//             document.write ((precio - desc2) + "<br>")
+//         }else{
+//             document.write(precio + "<br>")
+//         }
+//        precio= Number(prompt("Escriba el precio de otro producto o N para salir"));
+//     }
+//  }
+//  alert(descuento(precio, desc30, desc20, detener))
+
+
+
+
+
+
+
+
